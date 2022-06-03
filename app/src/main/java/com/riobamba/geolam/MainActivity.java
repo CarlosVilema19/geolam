ab3bbede1a7f840265e838299a237cf2e8517c86
CarlosVilema19 <carlosvilema14@gmail.com>
2022-06-03 11:24:03 -0500

    mapa1

--- app/src/main/java/com/riobamba/geolam/MainActivity.java
+++ app/src/main/java/com/riobamba/geolam/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.riobamba.geolam.modelo.ConexionMapa;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
