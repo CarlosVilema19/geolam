42419da5cd63908bb977212dbd0bb02dd55395b5
CarlosVilema19 <carlosvilema14@gmail.com>
2022-06-03 19:29:00 -0500

    Admin

---
+++ app/src/main/java/com/riobamba/geolam/GestionInformacionLugares.java
@@ -0,0 +1,14 @@
+package com.riobamba.geolam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestionInformacionLugares extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_informacion_lugares);
+    }
+}
