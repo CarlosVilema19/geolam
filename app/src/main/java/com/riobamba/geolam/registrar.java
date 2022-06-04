96af64367ef08ca6362949b4516fc02d5c88be67
MishellYerovi <mishellfriend@hotmail.com>
2022-06-04 11:41:30 -0500

    Ingresar una foto

--- app/src/main/java/com/riobamba/geolam/registrar.java
+++ app/src/main/java/com/riobamba/geolam/registrar.java
@@ -1,18 +1,25 @@
 package com.riobamba.geolam;
 
+import static android.content.Intent.createChooser;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
+import android.util.Base64;
+import android.app.ProgressDialog;
+import android.provider.MediaStore;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -22,14 +29,25 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Hashtable;
 import java.util.HashMap;
 import java.util.Map;
 
 public class registrar extends AppCompatActivity {
     EditText txtName, txtEmail, pass, txtApe, txtEdad, txtSexo;
-    Button btn_insert;
+    Button btnInsert;
     String[] items = {"Hombre", "Mujer"};
+
+    //Imagen
+    private Button btnCargarImagen;
+    private ImageView ivFoto;
+    private Bitmap bitmap;
+    private String claveImagen = "foto";
+    private String claveNombre = "nombre";
+    private int PICK_IMAGE_REQUEST = 1;
+
 
     //Items Sexo F y M
     AutoCompleteTextView autoCompleteTxtEdSexo;
@@ -39,6 +57,28 @@ public class registrar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+        //Imagen
+        btnCargarImagen = (Button) findViewById(R.id.btn_cargarfoto);
+        btnInsert = findViewById(R.id.btn_register);
+        ivFoto = findViewById(R.id.imageViewPerfil);
+
+        btnCargarImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (v == btnCargarImagen) {
+                    showFileChooser();
+                }
+
+                if (v == btnInsert) {
+                    showFileChooser();
+
+                }
+
+            }
+        });
+
 
         //Items Sexo F y M Autocomplete
         autoCompleteTxtEdSexo = (AutoCompleteTextView) findViewById(R.id.edsexo);
@@ -62,34 +102,62 @@ public class registrar extends AppCompatActivity {
         txtEdad = findViewById(R.id.ededad);
         txtSexo = findViewById(R.id.edsexo);
         pass = findViewById(R.id.etcontrasenia);
-        btn_insert = findViewById(R.id.btn_register);
+        btnInsert = findViewById(R.id.btn_register);
 
-        btn_insert.setOnClickListener(new View.OnClickListener() {
+        btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                //General
                 insertarUsusario();
+
             }
         });
     }
 
+    //Bitmap
+
+
+    public String getStringImagen(Bitmap bmp) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+
     private void insertarUsusario() {
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://qcqjfcit.lucusvirtual.es/insertar.php", new Response.Listener<String>() {
+        final ProgressDialog loading = ProgressDialog.show(this, "Subiendo ...", "Espere por favor");
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://qcqjfcit.lucusvirtual.es/insertar_.php", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                //Descartar el diálogo de progreso
+                loading.dismiss();
+                //Mostrando el mensaje de la respuesta
                 Toast.makeText(getApplicationContext(), "Operación Exitosa", Toast.LENGTH_SHORT).show();
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                //Descartar el diálogo de progreso
+                loading.dismiss();
+                //Showing toast
                 Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
             }
         }) {
             @Nullable
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
+                //Convertir bits a cadena
+                String imagen = getStringImagen(bitmap); //Imagen
+                //Obtener el nombre de la imagen
+                String nombreImagen = txtEmail.getText().toString().trim();
+
                 Map<String, String> parametros = new HashMap<String, String>();
+
                 parametros.put("email", txtEmail.getText().toString());
                 parametros.put("nombre_usuario", txtName.getText().toString());
                 parametros.put("apellido_usuario", txtApe.getText().toString());
@@ -97,12 +165,47 @@ public class registrar extends AppCompatActivity {
                 parametros.put("sexo", txtSexo.getText().toString());
                 parametros.put("contrasenia", pass.getText().toString());
 
+                //Imagen
+                parametros.put(claveImagen, imagen);
+                parametros.put(claveNombre, nombreImagen);
+
+
                 return parametros;
             }
         };
+        //Creación de una cola de solicitudes
         RequestQueue requestQueue = Volley.newRequestQueue(this);
+        //Agregar solicitud a la cola
         requestQueue.add(stringRequest);
     }
+
+    public void showFileChooser() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Selecciona la imagen"), PICK_IMAGE_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Cómo obtener el mapa de bits de la Galería
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Configuración del mapa de bits en ImageView
+                ivFoto.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 
     @Override
     public void onBackPressed() {
