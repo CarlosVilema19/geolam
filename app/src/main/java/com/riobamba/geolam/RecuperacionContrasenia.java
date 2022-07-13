cf77b90e817504335a4c90bb90e107da7d3d6ea2
MishellYerovi <mishellfriend@hotmail.com>
2022-07-13 15:06:05 -0500

    Iniciar sesión Administrador Recuperar contraseña administrador

--- app/src/main/java/com/riobamba/geolam/RecuperacionContrasenia.java
+++ app/src/main/java/com/riobamba/geolam/RecuperacionContrasenia.java
@@ -1,8 +1,12 @@
 package com.riobamba.geolam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,110 +16,316 @@ import android.widget.Toast;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputLayout;
 import com.riobamba.geolam.modelo.WebService;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RecuperacionContrasenia extends AppCompatActivity {
-EditText txtEmailUsuario;
+public class RecuperacionContrasenia extends AppCompatActivity  {
+    EditText txtEmailUsuario;
     String email;
-Button btnEnviarEmail;
-ProgressDialog progressdialog;
+    Button btnEnviarEmail;
+    ProgressDialog progressdialog;
+
+    int validarCorreo = 0;
+    int validarCorreo2 = 0;
+    TextInputLayout errorEmail;
+
+    String usuario2;
+    String admin2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperacion_contrasenia);
-        txtEmailUsuario=findViewById(R.id.etRecuperacion);
-        btnEnviarEmail=findViewById(R.id.btnEnviarEmail);
-        progressdialog=new ProgressDialog(this);
+        // setContentView(R.layout.activity_recuperacion_contrasenia);
+        Intent recibirUsuario = getIntent();
+        usuario2 = recibirUsuario.getStringExtra("usuario");
+
+        Intent recibirAdmin = getIntent();
+        admin2 = recibirAdmin.getStringExtra("admin");
+        //AlertDialog dialog = builder.create();
+
+
+        txtEmailUsuario = findViewById(R.id.etRecuperacion);
+        btnEnviarEmail = findViewById(R.id.btnEnviarEmail);
+        errorEmail = findViewById(R.id.txrecuperar);
+        progressdialog = new ProgressDialog(this);
+
+
         progressdialog.setMessage("Cargando...");
         progressdialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
+
+
+
         btnEnviarEmail.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+
+
+                errorEmail.setErrorIconDrawable(null);
+                errorEmail.setError(null);
+                email = txtEmailUsuario.getText().toString().trim();
                 progressdialog.show();
-             email = txtEmailUsuario.getText().toString().trim();
-                if(email.isEmpty())
-                {
+
+                if (email.isEmpty()) {
+
                     progressdialog.dismiss();
-                    txtEmailUsuario.setError("Ingrese un correo electrónico");
-                    txtEmailUsuario.requestFocus();
+                    //txtEmailUsuario.setError("Ingrese un correo electrónico");
+                    //txtEmailUsuario.requestFocus();
+                    errorEmail.setError("Ingrese un correo electrónico");
+                    errorEmail.requestFocus();
+                    errorEmail.setErrorIconDrawable(null);
 
-                }
-                else{
-                    String url = WebService.urlRaiz+WebService.servicioOlvideLaContrasenia;
+                } else {
+                    progressdialog.dismiss();
+                    //Toast.makeText(getApplicationContext(), admin2+usuario2, Toast.LENGTH_SHORT).show();
+                    if (admin2 != null) {
+                        validarCorreoBDAdmin();
+                        // progressdialog.dismiss();
+                        //Toast.makeText(getApplicationContext(), "éxito ad", Toast.LENGTH_SHORT).show();
 
-                    StringRequest stringRequest= new StringRequest(Request.Method.POST, url,
-                            //new Response.Listener<String>() {
-                                //@Override
-                                //public void onResponse(String response) {
-                            response ->
-                            {
-                                    try {
-
-                                        // parser_obj = new JSONParser();
-                                       //JSONArray array= (JSONArray) parser_obj.parse(response);
-                                       //JSONArray array = new JSONArray(response);
-                                       // JSONObject object =array.toJSONObject(array);
-                                        JSONObject object = new JSONObject( URLDecoder.decode( response, "UTF-8" ));
-                                        String mail=object.getString("mail");
-
-                                        if(mail.equals("send"))
-                                        {
-                                            progressdialog.dismiss();
-                                            Toast.makeText(getApplicationContext(), "Se ha enviado un correo de recuperación", Toast.LENGTH_SHORT).show();
-                                        }
-                                        else{
-                                            progressdialog.dismiss();
-                                            Toast.makeText(getApplicationContext(), "Correo inválido", Toast.LENGTH_SHORT).show();
-                                        }
-
-                                    }catch (JSONException | UnsupportedEncodingException e){
-                                       // e.printStackTrace();
-                                        progressdialog.dismiss();
-                                        Toast.makeText(getApplicationContext(), "Correo inválido", Toast.LENGTH_SHORT).show();
-                                    }
-                                }, error -> {
-                        //Toast.makeText(RecuperacionContrasenia.this, "Error -->" + error.toString(), Toast.LENGTH_SHORT).show();
+                    } else {
 
 
-                    //new Response.ErrorListener() {
-
-                       // @Override
-                        //public void onErrorResponse(VolleyError error) {
-                          //  Toast.makeText(getApplicationContext(), ""+error.toString(), Toast.LENGTH_SHORT).show();
-                        //}
-                    })
-
-                    {
-                        @Override
-                        protected Map<String,String> getParams() throws AuthFailureError{
-                            Map<String,String> enviarParametros=new HashMap<String, String>();
-                            enviarParametros.put("email",email);
-                            return  enviarParametros;
+                        if (usuario2 != null) {
+                            validarCorreoBDUsuario();
+                            // progressdialog.dismiss();
+                            //restablecerContraseniaUsuario();
+                            //Toast.makeText(getApplicationContext(), "éxito", Toast.LENGTH_SHORT).show();
                         }
-
-                    };
-
-                    RequestQueue requestQueue = Volley.newRequestQueue(RecuperacionContrasenia.this);
-                    requestQueue.add(stringRequest);
+                    }
                 }
-
             }
         });
     }
+
+    private void validarCorreoBDUsuario() {
+        RequestQueue queue2 = Volley.newRequestQueue(this);
+        String url2 = WebService.urlRaiz + WebService.servicioExistenciaCorreoRCUsuario;
+        StringRequest stringRequest2 = new StringRequest(Request.Method.POST, url2,
+
+                response ->
+                {
+                    try {
+
+                        JSONObject object = new JSONObject(URLDecoder.decode(response, "UTF-8"));
+                        String existencia = object.getString("valida");
+                        //Toast.makeText(getApplicationContext(), existencia, Toast.LENGTH_SHORT).show();
+                        if (existencia.equals("existe")) {
+                            //Toast.makeText(getApplicationContext(), "Existe", Toast.LENGTH_SHORT).show();
+                            // validarCorreo2=1;
+                            restablecerContraseniaUsuario();
+
+                        } else {
+                            if (existencia.equals("admin")) {
+                                //Toast.makeText(getApplicationContext(), "¡Cuenta de administrador!", Toast.LENGTH_SHORT).show();
+                                errorEmail.setError("¡Cuenta de administrador!");
+                                errorEmail.requestFocus();
+
+
+                            } else {
+
+                                if (existencia.equals("no_hay_registro")) {
+                                    errorEmail.setError("¡No existen coincidencias!");
+                                    errorEmail.requestFocus();
+                                    // Toast.makeText(getApplicationContext(), "Usuario no registrado", Toast.LENGTH_SHORT).show();
+
+                                }
+
+
+                            }
+
+                        }
+                        //  }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }, error -> {
+            Toast.makeText(this, "Error ", Toast.LENGTH_SHORT).show();
+
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> enviarParametros2 = new HashMap<String, String>();
+                enviarParametros2.put("email", txtEmailUsuario.getText().toString());
+                return enviarParametros2;
+            }
+        };
+
+        stringRequest2.setTag("REQUEST");
+        queue2.add(stringRequest2);
+        // return validarCorreo2;
+    }
+
+    private void validarCorreoBDAdmin() {
+        RequestQueue queue2 = Volley.newRequestQueue(this);
+        String url2 = WebService.urlRaiz + WebService.servicioExistenciaCorreoRC;
+        StringRequest stringRequest2 = new StringRequest(Request.Method.POST, url2,
+
+                response ->
+                {
+                    try {
+
+                        JSONObject object = new JSONObject(URLDecoder.decode(response, "UTF-8"));
+                        String existencia = object.getString("valida");
+                        //Toast.makeText(getApplicationContext(), existencia, Toast.LENGTH_SHORT).show();
+                        if (existencia.equals("existe")) {
+                            //Toast.makeText(getApplicationContext(), "Existe", Toast.LENGTH_SHORT).show();
+                            //validarCorreo=1;
+                            restablecerContraseniaAdmin();
+                        } else {
+                            if (existencia.equals("usuario")) {
+                                //Toast.makeText(getApplicationContext(), "¡Cuenta de usuario!", Toast.LENGTH_SHORT).show();
+                                errorEmail.setError("¡Cuenta de usuario!");
+                                errorEmail.requestFocus();
+
+                            } else {
+
+                                if (existencia.equals("no_hay_registro")) {
+                                    errorEmail.setError("¡Administrador no registrado!");
+                                    errorEmail.requestFocus();
+                                    //Toast.makeText(getApplicationContext(), "Administrador no registrado", Toast.LENGTH_SHORT).show();
+
+                                }
+
+
+                            }
+
+                        }
+                        // }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }, error -> {
+            Toast.makeText(this, "Error ", Toast.LENGTH_SHORT).show();
+
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> enviarParametros = new HashMap<String, String>();
+                enviarParametros.put("email", txtEmailUsuario.getText().toString());
+                return enviarParametros;
+            }
+        };
+        stringRequest2.setTag("REQUEST");
+        queue2.add(stringRequest2);
+        //return validarCorreo;
+    }
+
+    private void restablecerContraseniaAdmin() {
+        progressdialog.show();
+        RequestQueue queue2 = Volley.newRequestQueue(this);
+        String url = WebService.urlRaiz + WebService.servicioOlvideLaContraseniaAdmin;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+
+                response ->
+                {
+                    try {
+
+                        JSONObject object = new JSONObject(URLDecoder.decode(response, "UTF-8"));
+                        String mail = object.getString("mail");
+
+                        if (mail.equals("send")) {
+                            progressdialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Se ha enviado un correo de recuperación", Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+                            progressdialog.dismiss();
+                            errorEmail.setError("¡Correo inválido!");
+                            errorEmail.requestFocus();
+                            // Toast.makeText(getApplicationContext(), "Correo inválido", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } catch (JSONException | UnsupportedEncodingException e) {
+                        // e.printStackTrace();
+                        progressdialog.dismiss();
+                        errorEmail.setError("¡Correo inválido!");
+                        errorEmail.requestFocus();
+                        //Toast.makeText(getApplicationContext(), "Correo inválido", Toast.LENGTH_SHORT).show();
+                    }
+                }, error -> {
+
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> enviarParametros = new HashMap<String, String>();
+                enviarParametros.put("email", txtEmailUsuario.getText().toString());
+                return enviarParametros;
+            }
+
+        };
+
+        stringRequest.setTag("REQUEST");
+        queue2.add(stringRequest);
+    }
+
+    private void restablecerContraseniaUsuario() {
+        progressdialog.show();
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = WebService.urlRaiz + WebService.servicioOlvideLaContrasenia;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+
+                response ->
+                {
+                    try {
+
+                        JSONObject object = new JSONObject(URLDecoder.decode(response, "UTF-8"));
+                        String mail = object.getString("mail");
+
+                        if (mail.equals("send")) {
+                            progressdialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Se ha enviado un correo de recuperación", Toast.LENGTH_SHORT).show();
+                        } else {
+                            progressdialog.dismiss();
+                            errorEmail.setError("¡Correo inválido!");
+                            errorEmail.requestFocus();
+                            //Toast.makeText(getApplicationContext(), "Correo inválido", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } catch (JSONException | UnsupportedEncodingException e) {
+                        // e.printStackTrace();
+                        progressdialog.dismiss();
+                        errorEmail.setError("¡Correo inválido!");
+                        errorEmail.requestFocus();
+                        //Toast.makeText(getApplicationContext(), "Correo inválido", Toast.LENGTH_SHORT).show();
+                    }
+                }, error -> {
+
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> enviarParametros4 = new HashMap<String, String>();
+                enviarParametros4.put("email", txtEmailUsuario.getText().toString());
+                return enviarParametros4;
+            }
+
+        };
+
+        stringRequest.setTag("REQUEST");
+        requestQueue.add(stringRequest);
+    }
+
 }
--- app/src/main/java/com/riobamba/geolam/login_admin.java
+++ app/src/main/java/com/riobamba/geolam/login_admin.java
@@ -2,13 +2,186 @@ package com.riobamba.geolam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.common.hash.Hashing;
+import com.riobamba.geolam.modelo.WebService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class login_admin extends AppCompatActivity {
-
+    EditText edtUsuario, edtPassword;
+    TextInputLayout errorPass, errorEmail;
+    Button btnLogin, btnRecuperarAdmin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_admin);
+
+        errorPass = findViewById(R.id.txcontraseniaAdmin);
+        errorEmail=findViewById(R.id.txusuarioAdmin);
+        edtUsuario = findViewById(R.id.edusuarioAdmin);
+        edtPassword = findViewById(R.id.edcontraseniaAdmin);
+        btnLogin = findViewById(R.id.btniniciosesionAdmin);
+        btnRecuperarAdmin = findViewById(R.id.btnolvidarcontraseniaAdmin);
+
+
+
+        btnLogin.setOnClickListener(view -> {
+            validarUsuario();
+        });
+
+        btnRecuperarAdmin.setOnClickListener(v -> {
+            Intent intent = new Intent(login_admin.this, RecuperacionContrasenia.class);
+            intent.putExtra("admin","1");
+            startActivity(intent);
+        });
+
+
     }
+
+    private void validarUsuario(){
+        if(validarCamposVacios()==1) {
+            String url = WebService.urlRaiz + WebService.servicioValidarAdministrador;
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    response ->
+                    {
+
+                        try {
+                            //JSONArray array = new JSONArray(response);
+                            //JSONObject object = array.toJSONObject(array);
+                            JSONObject object = new JSONObject( URLDecoder.decode( response, "UTF-8" ));
+                            String existencia = object.getString("valida");
+                            //Toast.makeText(getApplicationContext(), existencia, Toast.LENGTH_SHORT).show();
+                            if (existencia.equals("existe")) {
+
+                                Toast.makeText(getApplicationContext(), "Bienvenido", Toast.LENGTH_SHORT).show();
+                                guardarEstadoButton();
+                                guardarEmail(edtUsuario.getText().toString());
+                                // Intent intent = new Intent(Login.this,Inicio.class);
+                                Intent intent = new Intent(login_admin.this, InicioAdmin.class);
+                                startActivity(intent);
+                            } else {
+                                if (existencia.equals("usuario"))
+                                {
+                                    Toast.makeText(getApplicationContext(), "Ingrese las credenciales de administrador", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    if(existencia.equals("error_contrasenia")){
+                                        //Toast.makeText(getApplicationContext(), "La contraseña es incorrecta", Toast.LENGTH_SHORT).show();
+                                        errorPass.setErrorIconDrawable(null);
+                                        errorPass.setError("La contraseña es incorrecta");
+                                        edtPassword.requestFocus();
+                                    }
+                                    else{
+                                        if(existencia.equals("no_hay_registro")){
+                                            Toast.makeText(getApplicationContext(), "Usuario no registrado", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                }
+
+                            }
+                        } catch (JSONException | UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                    },error ->{
+
+
+            })
+
+
+            {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> parametros = new HashMap<String, String>();
+                    parametros.put("email",edtUsuario.getText().toString());
+                    parametros.put("contrasenia",getSHA256(edtPassword.getText().toString()));
+
+                    return parametros;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        }
+
+    }
+    private int validarCamposVacios() {
+        errorEmail.setError(null);
+        errorPass.setError(null);
+        int camposVacios=0;
+        if (!edtUsuario.getText().toString().equals("") && !edtPassword.getText().toString().equals("")) {
+            camposVacios=1;
+        }
+        else {
+            if (edtUsuario.getText().toString().equals("") && edtPassword.getText().toString().equals("")) {
+
+                //edtUsuario.setError("Ingrese un correo electrónico");
+                //edtUsuario.requestFocus();
+
+                errorEmail.setError("Ingrese un correo electrónico");
+                errorEmail.requestFocus();
+                errorPass.setErrorIconDrawable(null);
+                errorPass.setError("Ingrese una contraseña");
+                //errorPass.requestFocus();
+            } else {
+                if (edtUsuario.getText().toString().equals("")) {
+                    errorEmail.setError("Ingrese un correo electrónico");
+                    errorEmail.requestFocus();
+                } else {
+                    if (edtPassword.getText().toString().equals("")) {
+                        errorPass.setErrorIconDrawable(null);
+                        errorPass.setError("Ingrese una contraseña");
+                        errorPass.requestFocus();
+                    }
+                }
+            }
+        }
+        return camposVacios;
+    }
+
+
+    public void guardarEstadoButton()
+    {
+        SharedPreferences preferences = getSharedPreferences("omitir_log", Context.MODE_PRIVATE);
+        boolean estado = true;
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("estado_inicio",estado);
+        editor.commit();
+
+    }
+
+    public String getSHA256(String data) {
+        return Hashing.sha256().hashString(data, StandardCharsets.UTF_8).toString();
+    }
+
+    public void guardarEmail(String email)
+    {
+        //SharedPreferences = getSharedPreferences("email", Context.MODE_PRIVATE);
+        SharedPreferences preferences = getSharedPreferences("correo_email",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("estado_correo", email);
+        editor.commit();
+
+    }
+
+
 }
