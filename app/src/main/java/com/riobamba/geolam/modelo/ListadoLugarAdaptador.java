435e6fc5fa00648abe32d4c27294e4145db3c40d
MishellYerovi <mishellfriend@hotmail.com>
2022-09-01 22:10:32 -0500

    Refrescar las imágenes

--- app/src/main/java/com/riobamba/geolam/modelo/ListadoLugarAdaptador.java
+++ app/src/main/java/com/riobamba/geolam/modelo/ListadoLugarAdaptador.java
@@ -127,12 +127,12 @@ public class ListadoLugarAdaptador extends RecyclerView.Adapter<ListadoLugarAdap
     public void onBindViewHolder(ListadoLugarAdaptador.ViewHolder viewHolder, int position) {
         ListadoLugar listadoLugar = lugarList.get(position);
         //Cargar Imagen
-        /*Glide.with(mCtx)
+        Glide.with(mCtx)
                 .load(listadoLugar.getImagenLugar())
-                        .into(viewHolder.imagenLugar);*/
-        Picasso.get().load(listadoLugar.getImagenLugar()).fit().centerCrop().networkPolicy(NetworkPolicy.NO_CACHE)
+                        .into(viewHolder.imagenLugar);
+        /*Picasso.get().load(listadoLugar.getImagenLugar()).fit().centerCrop().networkPolicy(NetworkPolicy.NO_CACHE)
                 .memoryPolicy(MemoryPolicy.NO_CACHE)
-                .into(viewHolder.imagenLugar);
+                .into(viewHolder.imagenLugar);*/
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
         viewHolder.nombreLugar.setText(listadoLugar.getNombreLugar());
